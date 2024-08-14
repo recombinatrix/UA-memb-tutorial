606196766d021a9387939f94e70f33f5159dfe61
recombinatrix <ada.s.quinn+github@gmail.com>
2024-08-14 16:43:20 +1000

    typo

--- run_equil_1000_to_10.sh
+++ run_equil_1000_to_10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a script to run equilibritation on an md system, slowly relating the position restriction over a number of runs.
+# This is a script to run equilibritation on an md system, slowly relaxing the position restraints over a number of runs.
 
 gmx grompp -f equil_memb_1000.mdp -c 13_GlyT2_POPC_CLR_water_ions_EM2.gro -r 13_GlyT2_POPC_CLR_water_ions_EM2.gro -n GlyT2_POPC_CLR.ndx -p GlyT2_POPC_CLR.top -o 14_GlyT2_POPC_CLR_EQ_1000.tpr  -maxwarn 2  2>&1 | tee 14_grompp.txt
 gmx mdrun -v -deffnm 14_GlyT2_POPC_CLR_EQ_1000
